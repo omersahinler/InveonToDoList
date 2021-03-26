7a8b994ab743f7d597af7f698f1c053162ca5db5
ömer şahinler <sahinleromer@gmail.com>
2021-03-26 22:08:50 +0300

    add migration datetime

--- InveonTodoList/Migrations/20210325063442_AddCloumnCreatDateTime.Designer.cs
+++ InveonTodoList/Migrations/20210325063442_AddCloumnCreatDateTime.Designer.cs
@@ -4,14 +4,16 @@ using InveonTodoList.Controllers.Infrastructure;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 namespace InveonTodoList.Migrations
 {
     [DbContext(typeof(ToDoContext))]
-    partial class ToDoContextModelSnapshot : ModelSnapshot
+    [Migration("20210325063442_AddCloumnCreatDateTime")]
+    partial class AddCloumnCreatDateTime
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ InveonTodoList/Migrations/20210325063442_AddCloumnCreatDateTime.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace InveonTodoList.Migrations
+{
+    public partial class AddCloumnCreatDateTime : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<DateTime>(
+                name: "CreatDateTime",
+                table: "ToDoList",
+                type: "datetime2",
+                nullable: false,
+                defaultValue: new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified));
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "CreatDateTime",
+                table: "ToDoList");
+        }
+    }
+}
