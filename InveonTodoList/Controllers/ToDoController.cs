92d43371ffb6c1f10a67de6434c4c2e3bdf1409f
ömer şahinler <sahinleromer@gmail.com>
2021-03-26 22:16:19 +0300

    Add Index, create,edit controller

--- InveonTodoList/Controllers/ToDoController.cs
+++ InveonTodoList/Controllers/ToDoController.cs
@@ -16,11 +16,69 @@ namespace InveonTodoList.Controllers
         {
             _context = context;
         }
-        public async Task<ActionResult> ToDoGetList()
+        public async Task<ActionResult> Index()
         {
             IQueryable<TodoList> items = from list in _context.ToDoList orderby list.Id select list;
             List<TodoList> mytodolist = await items.ToListAsync();
+
+            var  itemTime= await _context.ToDoList.FirstOrDefaultAsync(x => x.CreatDateTime == DateTime.Now);
+            if (itemTime != null)
+            {
+                TempData["Success2"] = "Girilen item zamanı gelmiştir!"+' '+itemTime.Content;
+            }
+            
             return View(mytodolist);
+        }
+        public IActionResult Create() => View();
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<ActionResult> Create(TodoList item)
+        {
+            if(ModelState.IsValid)
+            {
+                _context.Add(item);
+                await _context.SaveChangesAsync();
+                TempData["Success"] = "İtem Başarıyla Eklendi!";
+                return RedirectToAction("Index");
+            }
+            return View(item);
+        }
+        public async Task<ActionResult> Edit(int id)
+        {
+            TodoList item = await _context.ToDoList.FindAsync(id);
+            if (item == null)
+            {
+                return NotFound();
+            }
+            return View(item);
+        }
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<ActionResult> Edit(TodoList item)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Update(item);
+                await _context.SaveChangesAsync();
+                TempData["Success"] = "İtem Başarıyla Güncellendi!";
+                return RedirectToAction("Index");
+            }
+            return View(item);
+        }
+        public async Task<ActionResult> Delete(int id)
+        {
+            TodoList item = await _context.ToDoList.FindAsync(id);
+            if (item == null)
+            {
+                TempData["Error"] = "Böyle bir kayıt bulunmamıştır!";
+            }
+            else
+            {
+                _context.ToDoList.Remove(item);
+                await _context.SaveChangesAsync();
+                TempData["Success"] = "İtem Başarıyla Silinmiştir!";
+            }
+            return View(item);
         }
     }
 }
